25df77762729535b9fecc3ebfe94975defd5b9b3
张晨辉 <cheneyzhangchenhui@gmail.com>
2020-10-26 19:40:49 +0800

    feat: 增加请求频率限制

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
+import * as helmet from 'helmet'
+import * as csurf from 'csurf'
+import * as rateLimit from 'express-rate-limit'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -13,7 +16,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api-docs', app, document)
   app.enableCors()
-  app.use(cookieParser())
+  app
+    .use(cookieParser())
+    .use(helmet())
+    .use(csurf())
+    .use(
+      rateLimit({
+        windowMs: 60 * 1000, // 1 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+      }),
+    )
   await app.listen(3000)
 }
 
